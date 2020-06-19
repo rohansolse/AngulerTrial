3fc9ca4c04cee0e662744d22055da8c8dfd5d9c6
rohansolse <rohansolse@gmail.com>
2020-06-19 17:49:39 +0530

    part a done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Product } from "./product"
 
 @Component({
     selector: "app-root",
@@ -6,57 +7,21 @@ import { Component } from "@angular/core";
     styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-    // data : string = null
-    // passValue({target}){
-    //     let {value} = target
-    //     console.log(value)
-    //     this.data = value
-    // }
-    // heroList : string[] = []
-    // isValueAdded : Number = 0
-    // addHero(event){
-    //     let value = event.target.value
-    //     console.log(value)
-    //     event.target.value = ''
-    //     if(this.heroList.indexOf(value) == -1){
-    //         this.heroList.push(value)
-    //         this.isValueAdded = 1
-    //     }
-    //     else{
-    //         this.isValueAdded = 2
-    //     }
-    // }
-
-    // foodList : string[] = []
-    // constructor(){}
-    // addFruitInList(fruit){
-    //     console.log("food in the list : ", fruit)
-    //     this.foodList.push(fruit)
-    // }
-
-    // foodArray: string[]=[]
-    // insertFood(food){
-    //     if(this.foodArray.indexOf(food.target.value) == -1){
-    //         this.foodArray.push(food.target.value)
-    //         food.target.value = ''
-    //     }
-    // }
-    // deletedFood: string = null
-    // deleteFood(foodIndex){
-    //     console.log("foodindex : ",foodIndex)
-    //     this.deletedFood = this.foodArray[foodIndex]
-    //     this.foodArray = this.foodArray.filter((val,index)=> index != foodIndex)
-    // }
-
-    qty : number = 1
-    addQty(event){
-        this.qty = event.target.value
-        console.log("P qty : ", this.qty)
-        event.target.value = ''
-    }
-
-    updateQty(updatedQty){
-        console.log("updatedQty : ",updatedQty)
-        this.qty = updatedQty
-    }
+    data: Product[] = [
+        {
+            name : "mobile",
+            price : 12000,
+            qty : 2
+        },
+        {
+            name : "laptop",
+            price : 32000,
+            qty : 1
+        },
+        {
+            name : "bike",
+            price : 82000,
+            qty : 1
+        }
+    ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,41 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsComponent } from './forms/forms.component';
-import { SmallProjectComponent } from './small-project/small-project.component';
-import { NgStyleANDngClassComponent } from './ng-style-andng-class/ng-style-andng-class.component';
-import { NgifComponent } from './ngif/ngif.component';
-import { NgForComponent } from './ng-for/ng-for.component';
-import { NgSwitchComponent } from './ng-switch/ng-switch.component';
-import { NgSwitchAdvComponent } from './ng-switch-adv/ng-switch-adv.component';
-import { PipesTemPlateExpressionComponent } from './pipes-tem-plate-expression/pipes-tem-plate-expression.component';
-import { TemplateVariableComponent } from './template-variable/template-variable.component';
-import { HeaderComponent } from './header/header.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ChildComponent } from './child/child.component';
 import { ProductComponent } from './product/product.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormsComponent,
-    SmallProjectComponent,
-    NgStyleANDngClassComponent,
-    NgifComponent,
-    NgForComponent,
-    NgSwitchComponent,
-    NgSwitchAdvComponent,
-    PipesTemPlateExpressionComponent,
-    TemplateVariableComponent,
-    HeaderComponent,
-    ProfileComponent,
-    ChildComponent,
-    ProductComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductComponent,
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
